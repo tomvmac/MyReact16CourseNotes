c582bad6247a966f321db23039a2dde61cf884a9
Tom Mac <tmac@draftkings.com>
2018-08-12 13:48:32 -0400

    Updated Burger.js to handle when ingredients are empty and display text to user to add ingredients.

--- Apps/my-burger-builder/src/components/Burger/Burger.js
+++ Apps/my-burger-builder/src/components/Burger/Burger.js
@@ -6,15 +6,21 @@ const burger = (props) => {
 
     // Object.keys() give array of keys, transforms a key/value pair to array
     // https://javascript.info/keys-values-entries
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map(
                 (_, i) => {
                     return <BurgerIngredient key={igKey + i} type={igKey} />
                 }
             );
-        });  
+        })
+        .reduce ((arr, el) => {
+            return arr.concat(el)
+        }, []);  
 
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
