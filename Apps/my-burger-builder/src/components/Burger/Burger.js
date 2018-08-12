0fb5894e54c63eb2ada0a2be359aed47d6bd3375
Tom Mac <tmac@draftkings.com>
2018-08-12 13:39:07 -0400

    Added dynamic processing of ingredients from BurgerBuilder to Burger component.

--- Apps/my-burger-builder/src/components/Burger/Burger.js
+++ Apps/my-burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    // Object.keys() give array of keys, transforms a key/value pair to array
+    // https://javascript.info/keys-values-entries
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map(
+                (_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey} />
+                }
+            );
+        });  
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
 
         </div>
