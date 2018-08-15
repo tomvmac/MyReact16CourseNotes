4a96d96e6d47512ed146236f39f0b91d4b8ece44
Tom Mac <tmac@draftkings.com>
2018-08-15 19:40:09 -0400

    Added life cycle hooks to Modal to prevent it from re-rendering unnecessarily when it is not visble.

--- Apps/my-burger-builder/src/components/UI/Modal/Modal.js
+++ Apps/my-burger-builder/src/components/UI/Modal/Modal.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const modal = (props) => {
-    return (
-        <Aux>
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate(){
+        console.log("Modal.componentWillUpdate");
+    }
+
+    render() {
+        return (
+            <Aux>
             <Backdrop 
-                show={props.show}
-                clicked={props.modalClosed}
+                show={this.props.show}
+                clicked={this.props.modalClosed}
             />
             <div
                 className={classes.Modal}
                 style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1': '0'
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1': '0'
                 }}
                 >
-                {props.children}
+                {this.props.children}
             </div>
         </Aux>
-    );
-};
+        );
+    }
+}
 
-export default modal;
+export default Modal;
